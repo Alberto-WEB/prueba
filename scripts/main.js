756987333fba66f3fdac8e2c7d4de72ce46de740
Carlos <alberto.guzman.220793@gmail.com>
2020-08-21 15:03:41 -0500

    cambios en el nav responsive

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+   (function($) {
+  
+    
+    // Cierra el menú de respuesta cuando se hace clic en un enlace del nav
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+  })(jQuery);
+  
